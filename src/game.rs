25575dbfec805efb362a0d576de99c905e9a9137
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-02-04 21:54:35 -0300

    initial commit

---
+++ src/game.rs
@@ -0,0 +1,191 @@
+const WHITE_BG: &str = "\x1b[47m";
+const BLACK_FG: &str = "\x1b[30m";
+const RED_BG: &str   = "\x1b[41m";
+const RESET: &str    = "\x1b[0m";
+
+#[derive(Debug)]
+pub enum GameError {
+    OutOfBounds,
+    CursorIsNone,
+    InvalidDirection
+}
+
+pub struct Game {
+    board: Vec<Vec<bool>>,
+    dim_x: usize,
+    dim_y: usize,
+    cursor: Option<(usize, usize)>
+}
+
+impl Game {
+    pub fn new(dim_x: usize, dim_y: usize) -> Self {
+        let mut game = Game {
+            board: Vec::with_capacity(dim_x as usize),
+            dim_x,
+            dim_y,
+            cursor: None
+        };
+
+        for _ in 0..dim_x { 
+            game.board.push(vec![false; dim_y]);
+        }
+
+        game
+    }
+
+    pub fn print_game(&self) {
+        for y in 0..self.dim_y {
+            for x in 0..self.dim_x {
+                let cell = self.board[x][y];
+
+                match self.cursor {
+                    Some(coor) if coor == (x, y)  => {
+                        print!("{RED_BG}");
+
+                        if cell {
+                            print!("X");
+                        } else {
+                            print!("O");
+                        }
+                    },
+                    _ => {
+                        if cell {
+                            print!("{WHITE_BG}{BLACK_FG}X");
+                        } else {
+                            print!("O");
+                        }
+                    }
+                };
+
+                print!("{RESET} ");
+            }
+            println!("");
+        }
+    }
+
+    #[allow(dead_code)]
+    fn set_cell(&mut self, x: usize, y: usize) -> Result<(), GameError> {
+        if x >= self.dim_x || y >= self.dim_y {
+            return Err(GameError::OutOfBounds);
+        }
+
+        self.board[x][y] = true;
+        Ok(())
+    }
+
+    pub fn step_game(&mut self) {
+        let board = self.board.clone();
+
+        let bool_to_int = |b: bool| -> u32 {
+            if b { 1 } else { 0 }
+        };
+
+        for x in 0..self.dim_x {
+            for y in 0..self.dim_y {
+                let mut neigh = 0;
+
+                if x > 0 {
+                    if y > 0 {
+                        neigh += bool_to_int(board[x-1][y-1]);
+                    }
+
+                    neigh += bool_to_int(board[x-1][y]);
+
+                    if y < self.dim_y - 1 {
+                        neigh += bool_to_int(board[x-1][y+1]);
+                    }                        
+                }
+
+                if y > 0 {
+                    neigh += bool_to_int(board[x][y-1]);
+                }
+
+                if y < self.dim_y - 1 {
+                    neigh += bool_to_int(board[x][y+1]);
+                }                        
+
+                if x < self.dim_x - 1 {
+                    if y > 0 {
+                        neigh += bool_to_int(board[x+1][y-1]);
+                    }
+
+                    neigh += bool_to_int(board[x+1][y]);
+
+                    if y < self.dim_y - 1 {
+                        neigh += bool_to_int(board[x+1][y+1]);
+                    }                        
+                }
+
+                if !board[x][y] && neigh == 3 {
+                    self.board[x][y] = true;
+                } else if board[x][y] && (neigh < 2 || neigh > 3) {
+                    self.board[x][y] = false;
+                }
+            }
+        }
+    }
+
+    pub fn set_cursor(&mut self, x: usize, y: usize) -> Result<(), GameError> {
+        if x >= self.dim_x || y >= self.dim_y {
+            return Err(GameError::OutOfBounds);
+        }
+
+        self.cursor = Some((x, y));
+
+        Ok(())
+    }
+
+    pub fn move_cursor(&mut self, dir: char) -> Result<(), GameError> {
+        if let None = self.cursor {
+            return Err(GameError::CursorIsNone);
+        }
+
+        let mut cursor = self.cursor.unwrap();
+        
+        match dir {
+            'w' => {
+                if cursor.1 > 0 {
+                    cursor.1 -= 1;
+                }
+            },
+            'a' => {
+                if cursor.0 > 0 {
+                    cursor.0 -= 1;
+                }
+            },
+            's' => {
+                if cursor.1 < self.dim_y - 1 {
+                    cursor.1 += 1;
+                }
+            },
+            'd' => {
+                if cursor.0 < self.dim_x - 1 {
+                    cursor.0 += 1;
+                }
+            },
+            _ => {
+                return Err(GameError::InvalidDirection);
+            }
+        }
+
+        self.cursor = Some(cursor);
+
+        Ok(())
+    }
+
+    pub fn remove_cursor(&mut self) {
+        self.cursor = None;
+    }
+
+    pub fn swap_cell(&mut self) -> Result<(), GameError> {
+        if let None = self.cursor {
+            return Err(GameError::CursorIsNone);
+        }
+
+        let (x, y) = self.cursor.unwrap();
+
+        self.board[x][y] = !self.board[x][y];
+
+        Ok(())
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,102 @@
+mod game;
+
+use std::thread;
+use std::sync::mpsc::{self, Receiver};
+use std::io::{self, Read};
+use std::time::Duration;
+use termios::*;
+
+use game::Game;
+
+const SLEEP_DURATION: Duration = Duration::from_millis(100);
+
+fn spawn_input_thread(quit_char: char) -> Receiver<char> {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let mut input: [u8; 1] = [0;1];
+
+        while input[0] != (quit_char as u8) {
+            
+            if let Err(e) = io::stdin().read_exact(&mut input) {
+                println!("Error reading character: {}", e);
+            } else {
+                tx.send(input[0] as char).expect("Failed to send input character.");
+            }
+        }
+    });
+
+    rx
+}
+
+fn set_termios_lflag(t: &mut Termios, flag: u32) {
+    t.c_lflag = flag;
+
+    tcsetattr(0, TCSANOW, t).expect("Failed to set termios lflags.");
+}
+
+fn main() {
+    let mut game = Game::new(20, 10);
+
+    game.set_cursor(0, 0).expect("Cursor out of bounds.");
+
+    game.step_game();
+    game.print_game();
+
+    let mut termios = Termios::from_fd(0).expect("Failed to create termios structure for fd 0.");
+
+    let start_lflag = termios.c_lflag;
+    let input_lflag = termios.c_lflag & !(ICANON | ECHO);
+
+    set_termios_lflag(&mut termios, input_lflag);
+    
+    let rx = spawn_input_thread('q');
+
+    loop {
+        let input = rx.try_recv().unwrap_or('0');
+
+        match input {
+            'w'|'a'|'s'|'d' => game.move_cursor(input).unwrap(),
+            ' ' => game.swap_cell().unwrap(),
+            'q' => break,
+            _ => {}
+        };
+
+        print!("{}[2J", 27 as char);
+
+        game.print_game();
+
+        thread::sleep(SLEEP_DURATION);
+    }
+
+    game.remove_cursor();
+
+    let mut pause = false;
+
+    let rx = spawn_input_thread('q');
+    
+    loop {
+        let input = rx.try_recv().unwrap_or('0');
+
+        if input == 'q' {
+            break;
+        }
+
+        if input == 'p' {
+            pause = !pause;
+        }
+
+        if !pause || input == 'n' {
+            game.step_game();
+        }
+
+        print!("{}[2J", 27 as char);
+
+        game.print_game();
+
+        thread::sleep(SLEEP_DURATION);
+
+    }
+
+    set_termios_lflag(&mut termios, start_lflag);
+}
